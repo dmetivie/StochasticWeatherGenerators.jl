f8da6192cf6b496a7ce337687b11a08aee9282ae
David Métivier <46794064+dmetivie@users.noreply.github.com>
2023-04-13 16:34:07 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.32

---
+++ src/StochasticWeatherGenerator.jl
@@ -0,0 +1,5 @@
+module StochasticWeatherGenerator
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using StochasticWeatherGenerator
+using Test
+
+@testset "StochasticWeatherGenerator.jl" begin
+    # Write your tests here.
+end
