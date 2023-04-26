d87829fd7c6497b110ebf91edd4ea695fb88086a
David Métivier <46794064+dmetivie@users.noreply.github.com>
2023-04-26 15:58:18 +0200

    rm cartopy_map_with_stations

--- src/data/stations.jl
+++ src/data/stations.jl
@@ -29,56 +29,56 @@ end
 # * Plots * #
 
 ## * Plot map using PyPlot * ##
+#TODO: remove function because cause to much dependency 
+# """
+#     cartopy_map_with_stations(LON_idx, LAT_idx; station_name = nothing, value = nothing, fontsize=15, 
+#     precision_scale="50m", title_bar=nothing,
+#     LON_min=-5, # West
+#     LON_max=10, # Est
+#     LAT_min=41, # South
+#     LAT_max=52 # North
+#     )
+# Plot a [cartopy](https://scitools.org.uk/cartopy/docs/latest/) map (by default France) with point at the given Longitude and Latitude in  Decimal Degrees.
+# One can add station names and a value display to it.
+# """
+# function cartopy_map_with_stations(LON_idx, LAT_idx; station_name=nothing, value=nothing, fontsize=15,
+#     precision_scale="50m", title_bar=nothing,
+#     LON_min=-5, # West
+#     LON_max=10, # Est
+#     LAT_min=41, # South
+#     LAT_max=52 # North
+# )
+#     N = length(LON_idx)
 
-"""
-    cartopy_map_with_stations(LON_idx, LAT_idx; station_name = nothing, value = nothing, fontsize=15, 
-    precision_scale="50m", title_bar=nothing,
-    LON_min=-5, # West
-    LON_max=10, # Est
-    LAT_min=41, # South
-    LAT_max=52 # North
-    )
-Plot a [cartopy](https://scitools.org.uk/cartopy/docs/latest/) map (by default France) with point at the given Longitude and Latitude in  Decimal Degrees.
-One can add station names and a value display to it.
-"""
-function cartopy_map_with_stations(LON_idx, LAT_idx; station_name=nothing, value=nothing, fontsize=15,
-    precision_scale="50m", title_bar=nothing,
-    LON_min=-5, # West
-    LON_max=10, # Est
-    LAT_min=41, # South
-    LAT_max=52 # North
-)
-    N = length(LON_idx)
+#     fig, ax = PyPlot.subplots(figsize=(7, 7), subplot_kw=Dict("projection" => ccrs.PlateCarree()))
+#     ax.set_extent([LON_min, LON_max, LAT_min, LAT_max]) # LON_min, LON_max, LAT_min, LAT_max
+#     ax.add_feature(cfeature.COASTLINE.with_scale(precision_scale))
+#     ax.add_feature(cfeature.OCEAN.with_scale(precision_scale))
+#     ax.add_feature(cfeature.RIVERS.with_scale("10m"))
+#     ax.add_feature(cfeature.BORDERS.with_scale(precision_scale), linestyle="--")
 
-    fig, ax = PyPlot.subplots(figsize=(7, 7), subplot_kw=Dict("projection" => ccrs.PlateCarree()))
-    ax.set_extent([LON_min, LON_max, LAT_min, LAT_max]) # LON_min, LON_max, LAT_min, LAT_max
-    ax.add_feature(cfeature.COASTLINE.with_scale(precision_scale))
-    ax.add_feature(cfeature.OCEAN.with_scale(precision_scale))
-    ax.add_feature(cfeature.RIVERS.with_scale("10m"))
-    ax.add_feature(cfeature.BORDERS.with_scale(precision_scale), linestyle="--")
+#     # Value to display next to station 
+#     lat_shift = fill(-0.15, N)
 
-    # Value to display next to station 
-    lat_shift = fill(-0.15, N)
+#     lon_shift = fill(0.02, N)
 
-    lon_shift = fill(0.02, N)
+#     if value !== nothing
+#         sc = ax.scatter(LON_idx, LAT_idx, c=value, s=80, cmap=plt.cm.plasma, vmin=0)
+#         [ax.annotate("$(value[j])", (LON_idx[j] - lon_shift[j], LAT_idx[j] - lat_shift[j]), fontsize=fontsize, weight="bold") for j = 1:size(station_name, 1)]
+#         clb = fig.colorbar(sc, ax=ax, fraction=0.046, pad=0.04)
+#     else
+#         sc = ax.scatter(LON_idx, LAT_idx, s=80, cmap=plt.cm.plasma, vmin=0)
+#     end
 
-    if value !== nothing
-        sc = ax.scatter(LON_idx, LAT_idx, c=value, s=80, cmap=plt.cm.plasma, vmin=0)
-        [ax.annotate("$(value[j])", (LON_idx[j] - lon_shift[j], LAT_idx[j] - lat_shift[j]), fontsize=fontsize, weight="bold") for j = 1:size(station_name, 1)]
-        clb = fig.colorbar(sc, ax=ax, fraction=0.046, pad=0.04)
-    else
-        sc = ax.scatter(LON_idx, LAT_idx, s=80, cmap=plt.cm.plasma, vmin=0)
-    end
+#     if title_bar !== nothing
+#         clb.ax.set_title(title_bar)
+#     end
 
-    if title_bar !== nothing
-        clb.ax.set_title(title_bar)
-    end
+#     if station_name !== nothing
+#         [ax.annotate(station_name[j], (LON_idx[j] - 30lon_shift[j], LAT_idx[j] + 2.5lat_shift[j]), fontsize=fontsize / 1.3, weight="bold") for j in 1:N]
+#     end
 
-    if station_name !== nothing
-        [ax.annotate(station_name[j], (LON_idx[j] - 30lon_shift[j], LAT_idx[j] + 2.5lat_shift[j]), fontsize=fontsize / 1.3, weight="bold") for j in 1:N]
-    end
-
-    ax.set_aspect("auto")
-    PyPlot.tight_layout(h_pad=1)
-    return fig
-end
+#     ax.set_aspect("auto")
+#     PyPlot.tight_layout(h_pad=1)
+#     return fig
+# end
