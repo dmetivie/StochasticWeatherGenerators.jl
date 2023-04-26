03be5cb3892798c76b98b8b892965f60420f6915
David Métivier <46794064+dmetivie@users.noreply.github.com>
2023-04-26 13:33:49 +0200

    main

--- src/StochasticWeatherGenerator.jl
+++ src/StochasticWeatherGenerator.jl
@@ -1,5 +1,46 @@
 module StochasticWeatherGenerator
 
-# Write your package code here.
+using Distributions
+# Trigonometric part
+
+using ArgCheck
+using LogExpFunctions: logsumexp!
+import BinaryHierarchicalPeriodicHiddenMarkovModels: polynomial_trigo, interleave2
+import Distributions: fit_mle
+using Optimization
+using JuMP, Ipopt
+
+# 
+
+abstract type AbstractMLE end
+
+include("TrigonometricMixture/utilities.jl")
+include("TrigonometricMixture/fit_mle_trig_EM.jl")
+include("TrigonometricMixture/fit_mle_trig_Optim.jl")
+
+export OptimMLE
+export μₜ, σₜ, αₜ
+
+using StatsBase
+using DataFrames, DataFramesMeta, Dates
+using CSV, Printf # File Read/Load
+using LinearAlgebra: tril 
+using Copulas # for rain generation
+# Data part
+include("utilities.jl")
+include("data/extracting.jl")
+include("data/stations.jl")
+
+# Rain part
+include("rain/correlations.jl")
+include("rain/generation.jl")
+
+export rand_rain
+export joint_rain, Σ_Spearman2Pearson, Σ_Kendall2Pearson, corTail
+export select_in_range_df, collect_data_ECA, shortname
+export cartopy_map_with_stations, distance_x_to_y, distance_x_to_all_stations, dms_to_dd
+export vec_triu, onefy, zerofy
+export whole_year, dayofyear_Leap
+export my_color
 
 end
