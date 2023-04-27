a0937a0fe04238ef7be76243e0e7111285904d53
David Métivier <46794064+dmetivie@users.noreply.github.com>
2023-04-27 11:29:24 +0200

    fix rand_rain (from wrong copy paste)

--- src/rain/generation.jl
+++ src/rain/generation.jl
@@ -1,24 +1,25 @@
 """
     rand_rain(mixs::AbstractArray{<:MixtureModel}, n2t::AbstractVector, z::AbstractVector, y::AbstractMatrix, Σk::AbstractArray)
 Generate a (nonhomegenous) sequence of length `length(n2t)` of rain amounts conditionally to a given dry/wet matrix `y` and (hidden) state sequence `z`.
-Univariate distribution are given by `mixs` while correlations are given by Σk.
+Univariate distribution are given by `mixs` while correlations are given by covariance matrix Σk.
 """
-function rand_rain(mixs::AbstractArray{<:MixtureModel}, n2t::AbstractVector, z::AbstractVector, y::AbstractMatrix, Σk::AbstractArray)
+function rand_rain(mixs::AbstractArray{<:MixtureModel}, n2t::AbstractVector, z::AbstractVector, y::AbstractArray, Σk::AbstractArray)
     D, N = size(y)
 
-    r = zeros(D, N)
-
-    for n in 1:N
-        nz_r = findall(!iszero, y[:, n]) # Station j with precipitation
+    r = zeros(D, N) # could be specific to `eltype` of mix
+	
+    for (n, t) in enumerate(n2t)
+        nz_r = findall(!iszero, y[:,n]) # Station j with precipitation
         if length(nz_r) == 0
             continue
-        elseif length(nz_r) == 1 #|| z[n] == K
+        elseif length(nz_r) == 1 #|| z[n] == K (in a old version I did that because dry weather regime z = K was expected to be uncorrelated regime)
             for j in nz_r
-                r[j, n] = rand(mixs[z[n], n2t[n], j])
+                r[j, n] = rand(mixs[z[n], t, j])
             end
         else
-            Σnz = Σk[z[n]][nz_r, nz_r]
-            r[nz_r, n] = rand(GaussianCopula(Σnz))
+			C = GaussianCopula(Σk[z[n]][nz_r, nz_r]) #! use cor2cov if you just have correlations matrix !!! (#? in practice does it make a difference?)
+			D = SklarDist(C, tuple(mixs[z[n], t, nz_r]...)) # The final distribution
+            r[nz_r, n] = rand(D)
         end
     end
     return r
