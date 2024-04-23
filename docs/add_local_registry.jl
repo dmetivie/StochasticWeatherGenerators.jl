caf058f469365702aa8bccafec94f9d5924c4d0f
David Métivier <46794064+dmetivie@users.noreply.github.com>
2024-04-23 13:21:11 +0200

    try doc ci with local registry

---
+++ docs/add_local_registry.jl
@@ -0,0 +1,6 @@
+# Add PackageNursery registry for CI
+# https://github.com/j-fu/LiquidElectrolytes.jl/blob/main/docs/addnursery.jl
+# https://discourse.julialang.org/t/hosting-docs-with-documenter-jl-with-dependencies-github-actions-with-unregistered-packages/83364/9?u=dmetivie
+using Pkg
+Pkg.Registry.add("General")
+Pkg.Registry.add(RegistrySpec(url = "https://github.com/dmetivie/MyJuliaRegistry/"))
