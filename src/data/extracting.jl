ecd6ea063f3467a4e033f6af3eb6e756b047079d
David Métivier <46794064+dmetivie@users.noreply.github.com>
2025-01-14 17:01:26 +0100

    add meteoFrance

--- src/data/extracting.jl
+++ src/data/extracting.jl
@@ -17,12 +17,12 @@ select_in_range_df(data, start_Date, interval_Date, portion) = sort(findall(df -
     collect_data_ECA(STAID::Integer, path::String, var::String="RR"; skipto=19, header = 18) 
 `path` gives the path where all data files are stored in a vector
 """
-function collect_data_ECA(STAID::Integer, path::String, var::String="RR"; skipto=19, header = 18, url = false) 
+function collect_data_ECA(STAID::Integer, path::String, var::String="RR"; skipto=19, header=18, url=false)
     file = url ? Base.download(string(path, @sprintf("STAID%06.d.txt", STAID))) : joinpath(path, string("ECA_blend_$(lowercase(var))/$(uppercase(var))_", @sprintf("STAID%06.d.txt", STAID)))
     if isfile(file)
-        return CSV.read(file, DataFrame, skipto=skipto, header = header, comment="#", normalizenames=true, dateformat="yyyymmdd", types=Dict(:DATE => Date))
+        return CSV.read(file, DataFrame, skipto=skipto, header=header, comment="#", normalizenames=true, dateformat="yyyymmdd", types=Dict(:DATE => Date))
     else
-       return @warn "STAID $STAID File does not exists $(file)"
+        return @warn "STAID $STAID File does not exists $(file)"
     end
 end
 
@@ -35,14 +35,14 @@ end
 - `skipto` and `header` for `csv` files with meta informations/comments at the beginning of files. See `CSV.jl`.
 - `return_nothing` if `true` it will return `nothing` is the file does not exists or does not have enough valid data.
 """
-function collect_data_ECA(STAID, date_start::Date, date_end::Date, path::String, var::String="RR"; portion_valid_data=1, skipto=19, header = 18, return_nothing = true, url = false)
+function collect_data_ECA(STAID, date_start::Date, date_end::Date, path::String, var::String="RR"; portion_valid_data=1, skipto=19, header=18, return_nothing=true, url=false)
     @assert 0 ≤ portion_valid_data ≤ 1
-    data = collect_data_ECA(STAID, path, var; skipto=skipto, header = header, url = url)
+    data = collect_data_ECA(STAID, path, var; skipto=skipto, header=header, url=url)
     total_time = length(date_start:Day(1):date_end)
     if data isa DataFrame
         @subset!(data, date_start .≤ :DATE .≤ date_end)
         enougrow = nrow(data) ≥ total_time * portion_valid_data
-        NONvalidrow = count(!iszero, data[:, Symbol(string("Q_",var))])
+        NONvalidrow = count(!iszero, data[:, Symbol(string("Q_", var))])
         enougrow ? nothing : @warn "STAID $(data.STAID[1]) nrow = $(nrow(data)) < total_time = $(total_time * portion_valid_data)."
         NONvalidrow == 0 ? nothing : @warn "STAID $(data.STAID[1]) There are $NONvalidrow missing rows."
         if return_nothing
@@ -59,7 +59,7 @@ function collect_data_ECA(STAID, date_start::Date, date_end::Date, path::String,
     end
 end
 
-function collect_data_ECA!(data, path::String, var; validity = false)
+function collect_data_ECA!(data, path::String, var; validity=false)
     STAID = [data[j][!, :STAID][1] for j in eachindex(data)]
     col = validity ? [:DATE, Symbol(var), Symbol(string("Q_", uppercase(var)))] : [:DATE, Symbol(var)]
     for (j, i) in enumerate(STAID)
@@ -101,16 +101,12 @@ end
 
 """
     collect_data_INRAE(station_path::String; show_warning=false, impute_missing=[])
-    collect_data_INRAE(df_full::DataFrame; show_warning=false, impute_missing=[])
 Read from a file an INRAE formatted weather station data and transform it to match ECA standard naming conventions.
-Possibility to impute missing with `Impute.Interpolate` for specified columns e.g. `impute_missing=[:TX]`.
+- `impute_missing` expects a vector of column name(s) where to impute missing with `Impute.Interpolate` e.g. `impute_missing=[:TX]`.
+- `show_warning` in case of missing data. `false` for no column, `true` for all variables columns and for selected columns e.g. `show_warning = [:TX]`.
 """
 function collect_data_INRAE(station_path::String; show_warning=false, impute_missing=[])
     df_full = CSV.read(station_path, DataFrame, header=13)
-    collect_data_INRAE(df_full; show_warning=show_warning, impute_missing=impute_missing)
-end
-
-function collect_data_INRAE(df_full::DataFrame; show_warning=false, impute_missing=[])
     df = @chain df_full begin
         @transform(:STAID = :NUM_POSTE)
         @transform(:DATE = Date.(:AN, :MOIS, :JOUR))
@@ -118,6 +114,17 @@ function collect_data_INRAE(df_full::DataFrame; show_warning=false, impute_missi
         @transform(:QQ = :RG / 100)
         DataFramesMeta.@select :STAID :DATE :TN :TX :QQ :ETPP :RR :CO2
     end
+    return clean_data(df; show_warning=show_warning, impute_missing=impute_missing)
+end
+
+"""
+    clean_data(df::DataFrame; show_warning=false, impute_missing=[])
+Impute missing and show warning for missings.
+It assumes that the first two columns are not numeric.
+- `impute_missing` expects a vector of column name(s) where to impute missing with `Impute.Interpolate` e.g. `impute_missing=[:TX]`.
+- `show_warning` in case of missing data. `false` for no column, `true` for all variables columns and for selected columns e.g. `show_warning = [:TX]`.
+"""
+function clean_data(df::DataFrame; show_warning=false, impute_missing=[])
     if show_warning != false
         show_warning = show_warning == true ? names(df)[3:ncol(df)] : show_warning
         for c in show_warning
@@ -140,3 +147,52 @@ function collect_data_INRAE(df_full::DataFrame; show_warning=false, impute_missi
     return df
 end
 
+const meteofrance_id_to_url = "https://www.data.gouv.fr/fr/datasets/r/966dcd99-2856-44d0-8a2d-0b1a1c5ee737"
+
+df_api_url = CSV.read(Base.download(meteofrance_id_to_url), DataFrame)
+
+"""
+    download_data_MeteoFrance(STAID, period = "1950-2021", variables = "all")
+- Option for `period` are "1846-1949", "1950-2021", "2022-2023"
+- Option for `variables` are `all`, "RR-T-Wind", "others"
+"""
+function download_data_MeteoFrance(STAID; period="1950-2021", variables = "all")
+    dep = string(STAID)[1:2]
+    if variables != "all"
+        if variables == "R-T-Wind"
+            varFR = "RR-T-Vent"
+        elseif variables == "others"
+            varFR = "autres-parametres"
+        else
+            return "Check spellin"
+        end
+        id_name = string("QUOT_departement_", dep, "_periode_", period, "_", varFR)
+        id = @subset(df_api_url, :title .== id_name)[1,:id]
+        path = string("https://tabular-api.data.gouv.fr/api/resources/$(id)/data/csv/?NUM_POSTE__exact=", STAID)
+        return df = CSV.read(Base.download(path), DataFrame)
+    else
+        dfRTW = download_data_MeteoFrance(STAID; period=period, variables = "R-T-Wind")
+        dfoth = download_data_MeteoFrance(STAID; period=period, variables = "others")
+        return leftjoin!(dfRTW, dfoth[:,7:end], on = :AAAAMMJJ)
+    end
+end
+
+"""
+    collect_data_MeteoFrance(STAID; show_warning=false, impute_missing=[], period="1950-2021", variables = "all")
+Given a `STAID` (station ID given by Météo France), it returns a `DataFrame` with data in `period` and for the `variables`.
+- `STAID` can be an integer or string.
+- Option for `period` are "1846-1949", "1950-2021", "2022-2023"
+- Option for `variables` are `all`, "RR-T-Wind", "others"
+- `impute_missing` expects a vector of column name(s) where to impute missing with `Impute.Interpolate` e.g. `impute_missing=[:TX]`.
+- `show_warning` in case of missing data. `false` for no column, `true` for all variables columns and for selected columns e.g. `show_warning = [:TX]`.
+The data is available through the French [Data.gouv.fr](https://www.data.gouv.fr/en/) website api. Data may be updated without notice.
+See the following two links to get informations on the "RR-T-Wind" and "others" variables (in French)
+- https://object.files.data.gouv.fr/meteofrance/data/synchro_ftp/BASE/QUOT/Q_descriptif_champs_RR-T-Vent.csv
+- https://object.files.data.gouv.fr/meteofrance/data/synchro_ftp/BASE/QUOT/Q_descriptif_champs_autres-parametres.csv
+Or the the [SICLIMA website](https://agroclim.inrae.fr/siclima/help/references.html) with information (in French) about computation and conversion for some weather variables/index.
+"""
+function collect_data_MeteoFrance(STAID; show_warning=false, impute_missing=[], kwargs...)
+    df_full = download_data_MeteoFrance(STAID; kwargs...)
+    rename!(df_full, :NUM_POSTE => :STAID, :NOM_USUEL => :STANAME, :AAAAMMJJ => :DATE)#, :GLOT => :QQ)
+    return clean_data(df_full; show_warning=show_warning, impute_missing=impute_missing)
+end
