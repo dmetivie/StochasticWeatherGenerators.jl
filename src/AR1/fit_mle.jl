bcf7ae56c00d634bb5b2caf3e0e7b0db129103d5
David Métivier <46794064+dmetivie@users.noreply.github.com>
2025-04-15 10:18:05 +0200

    fix AR1 fit

--- src/AR1/fit_mle.jl
+++ src/AR1/fit_mle.jl
@@ -11,15 +11,25 @@ The correspondance between day of the year `t` and index in the time series `n` 
 
 with ``\\xi \\sim \\mathcal{N}(0,1)``.
 """
-function fit_AR1(df_full::DataFrame, var, 𝐃𝐞𝐠, K = length(unique(df_full.z |> skipmissing)), T = length(unique(dayofyear_Leap.(df_full.DATE))))
-    df = dropmissing(df_full[:, [:DATE, var, :z]])
-    z = df.z
-    n2t = dayofyear_Leap.(df.DATE)
-    y = df[:, var]
-    return fit_AR1(y, z, n2t, 𝐃𝐞𝐠, T)
+function fit_AR1(df_full::DataFrame, var, 𝐃𝐞𝐠, T = length(unique(dayofyear_Leap.(df_full.DATE))))
+    if "z" ∈ names(df_full)
+        df = dropmissing(df_full[:, [:DATE, var, :z]])
+        z = df.z
+        K = length(unique(z |> skipmissing))
+        n2t = dayofyear_Leap.(df.DATE)
+        y = df[:, var]
+        return fit_AR1(y, z, n2t, 𝐃𝐞𝐠, K, T)
+    else
+        df = dropmissing(df_full[:, [:DATE, var]])
+        n2t = dayofyear_Leap.(df.DATE)
+        y = df[:, var]
+        n2t = dayofyear_Leap.(df.DATE)
+        y = df[:, var]
+        return fit_AR1(y, n2t, 𝐃𝐞𝐠, T)
+    end
 end
 
-function fit_AR1(y::AbstractArray, z::AbstractArray, n2t, 𝐃𝐞𝐠, K = length(unique(z |> skipmissing)), T = length(unique(n2t)))
+function fit_AR1(y::AbstractVector, z::AbstractVector, n2t, 𝐃𝐞𝐠, K, T)
     n_in_t_k = [setdiff(findall(.&(n2t .== t, z .== k)), 1) for t in 1:T, k in 1:K]
 
     model_AR1_JuMP = model_for_loglikelihood_AR1(𝐃𝐞𝐠, T, silence=true)
@@ -40,6 +50,26 @@ function fit_AR1(y::AbstractArray, z::AbstractArray, n2t, 𝐃𝐞𝐠, K = leng
     end
 
     return AR1([μₜ(t, θ_μ[k, :], T) for k in 1:K, t in 1:T], [ρₜ(t, θ_ρ[k, :], T) for k in 1:K, t in 1:T], [σₜ(t, θ_σ[k, :], T) for k in 1:K, t in 1:T])
+end
+
+function fit_AR1(y::AbstractVector, n2t, 𝐃𝐞𝐠, T)
+    n_in_t = [setdiff(findall(.&(n2t .== t)), 1) for t in 1:T]
+
+    model_AR1_JuMP = model_for_loglikelihood_AR1(𝐃𝐞𝐠, T, silence=true)
+    θ_μ, θ_ρ, θ_σ = zeros(2𝐃𝐞𝐠 + 1), zeros(2𝐃𝐞𝐠 + 1), zeros(2𝐃𝐞𝐠 + 1)
+
+    observable = Dict{Symbol,Vector}()
+
+    observable[:N] = [length(n_in_t[t]) for t in 1:T]
+    observable[:∑yₜ²] = [sum(y[i]^2 for i in n_in_t[t]; init=0) for t in 1:T]
+    observable[:∑yₜ₋₁²] = [sum(y[i-1]^2 for i in n_in_t[t]; init=0) for t in 1:T]
+    observable[:∑yₜyₜ₋₁] = [sum(y[i] * y[i-1] for i in n_in_t[t]; init=0) for t in 1:T]
+    observable[:∑yₜ] = [sum(y[i] for i in n_in_t[t]; init=0) for t in 1:T]
+    observable[:∑yₜ₋₁] = [sum(y[i-1] for i in n_in_t[t]; init=0) for t in 1:T]
+
+    initialvalue_optimize!(θ_μ, θ_ρ, θ_σ, model_AR1_JuMP, observable; warm_start=true)
+
+    return AR1([μₜ(t, θ_μ, T) for t in 1:T], [ρₜ(t, θ_ρ, T) for t in 1:T], [σₜ(t, θ_σ, T) for t in 1:T])
 end
 
 """
